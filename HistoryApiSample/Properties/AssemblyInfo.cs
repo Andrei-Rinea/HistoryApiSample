2559827571f1e4cdef717291104e994f4a52ce67
Andrei Rînea <andrei@rinea.ro>
2016-07-18 16:46:26 +0300

    Fixed renaming and packages location

--- HistoryApiSample/Properties/AssemblyInfo.cs
+++ HistoryApiSample/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication2")]
+[assembly: AssemblyTitle("HistoryApiSample")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WebApplication2")]
+[assembly: AssemblyProduct("HistoryApiSample")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
